a367c38bafbda696b74ebb4c6564ceb2d2c0eb73
ambareesha.av <ambareesha.av@aurut.com>
2022-05-25 12:30:08 +0530

    initial commit

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class DashBoard extends StatefulWidget {
+  const DashBoard({Key? key}) : super(key: key);
+
+  @override
+  State<DashBoard> createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: const Center(
+        child: Text('Dashboard'),
+      ),
+    );
+  }
+}
